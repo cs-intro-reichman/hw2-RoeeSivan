92a0a8a91107599c94e0121d23c1caf92c19cceb
Roee Sivan <roee.sivan@post.runi.ac.il>
2024-11-16 00:41:41 +0200

    CalcPi

--- CalcPi.java
+++ CalcPi.java
@@ -1,27 +1,19 @@
-// Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
 	    // Replace this comment with your code
 		int terms = Integer.parseInt(args[0]);
-		int sign=0;
-		int denominator=1; //מכנה
-		int counter=1;//up
-		double proxy=0;//pi approximated
-		for(int i =0;i<terms;terms--)
+		double proxy=0.0;//pi approximated
+		if(terms==1)
 		{
-			if(terms%2==0)
-			{
-				proxy+=(double)counter/denominator;
-				denominator+=2;
-			}
-			if(terms%2!=0)
-			{
-			 proxy-=(double)counter/denominator;
-			 denominator+=2;
-			}
+			proxy=1.0;
+			System.out.println("pi according to Java: " + Math.PI);
+			System.out.println("pi, approximated:     " + (proxy * 4)); 
 		}
-		System.out.println("pi according to Java:     "+Math.PI);
-		System.out.println("pi, approximated:     "+proxy*4);
-		
+		for(int i =1;i<=terms;i++)// i will be the index of the series as i go on
+		{
+			proxy+=Math.pow(-1,i+1)/(2*i-1);
+		}
+		System.out.println("pi according to Java: " + Math.PI);
+		System.out.println("pi, approximated:     " + (proxy * 4.0)); 
 	}
 }
